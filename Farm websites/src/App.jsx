d24ab047139373eac7568fae0595485bb79ea13c
Agyemang8172 <godfredagyemang31052005@gmail.com>
2025-11-30 00:02:21 +0000

    first commit

---
+++ Farm websites/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import sidebar from './Components/sidebar'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
---
+++ Farm websites/src/Components/sidebar.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const sidebar = () => {
+  return (
+    <aside>
+       
+            <img src='Farm websites/public/icons/fluent_power-24-filled.png'  />
+
+            <div>
+                <div>
+                    <img src='' />
+                    <p></p>
+                </div>
+                <div>
+                    <img src='' />
+                    <p></p>
+                </div>
+                <div>
+                    <img src='' />
+                    <p></p>
+                </div>
+                <div>
+                    <img src='' />
+                    <p></p>
+                </div>
+                <div>
+                    <img src='' />
+                    <p></p>
+                </div>
+            </div>
+
+         <div>
+            <h1>Customer Support</h1>
+
+            <p>Ask you query , place
+                requests or important
+                issues. Our support 
+                team will contact 24/7 
+                to you.</p>
+
+                <button><img src=''/>  Connect Now</button>
+         </div>
+
+
+            <div>
+                <p>Terms & Services</p>
+                <p>Privacy Policy</p>
+            </div>
+     
+
+      
+    </aside>
+  )
+}
+
+export default sidebar
